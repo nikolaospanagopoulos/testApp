9fcffd4bdc9e7f0aef8df172d3af201ddb3bb7eb
nikolaospanagopoulos <nikos4222@outlook.com.gr>
2022-07-01 20:15:51 +0300

    devide and add

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+int add(int num1, int num2);
+int devide(int num1, int num2);
 int main() {
   std::cout << "hello" << std::endl;
+
+  std::cout << add(5, 4) << std::endl;
+
+  std::cout << devide(10, 2) << std::endl;
+
   return 0;
 }
+
+int add(int num1, int num2) { return num1 + num2; }
+int devide(int num1, int num2) { return num1 / num2; }
